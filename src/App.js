4ed8e9c72f9b7616a793e566383bd5279af23e29
Matheus Marques <marquesm91@gmail.com>
2018-01-11 12:04:37 -0200

    feat: Add react-router-dom to project to have a better URL address interaction

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom';
 import ListBooks from './view/ListBooks';
 import SearchBooks from './view/SearchBooks';
 
 import './App.css'
 
 class MyReadsApp extends Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks onMainPage={() => this.setState({ showSearchPage: false })} />
-        ) : (
-          <ListBooks onSearchPage={() => this.setState({ showSearchPage: true })} />
-        )}
+        <Route exact path='/' component={ListBooks} />
+        <Route path='/search' component={SearchBooks} />
       </div>
     )
   }
--- src/view/ListBooks.js
+++ src/view/ListBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Shelf, HeaderPage } from '../components';
 
 const books = [
@@ -56,7 +57,7 @@ class ListBooks extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={this.props.onSearchPage}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     );
