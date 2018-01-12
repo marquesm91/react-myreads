879d925e6bf201ae7985806c3ac5ab9b78d57c90
Matheus Marques <marquesm91@gmail.com>
2018-01-12 12:15:27 -0200

    refactor: Change SearchBar to stateless function component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,38 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class SearchBar extends Component {
-  state = {
-    query: ''
-  }
+const SearchBar = ({ onUpdateQuery, query, placeholder }) => (
+  <div className="search-books-bar">
+    <Link to='/' className='close-search'>Close</Link>
+    <div className="search-books-input-wrapper">
+      {/*
+        NOTES: The search from BooksAPI is limited to a particular set of search terms.
+        You can find these search terms here:
+        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-  updateQuery = query => {
-    this.setState({ query: query.trim() });
-  }
-
-  render() {
-    return (
-      <div className="search-books-bar">
-        <Link to='/' className='close-search'>Close</Link>
-        <div className="search-books-input-wrapper">
-          {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-          <input
-            type="text"
-            placeholder={this.props.placeholder}
-            value={this.state.query}
-            onChange={event => this.updateQuery(event.target.value)}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+        you don't find a specific author or title. Every search is limited by search terms.
+      */}
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={query}
+        onChange={event => onUpdateQuery(event.target.value)}
+      />
+    </div>
+  </div>
+);
 
 export { SearchBar };
