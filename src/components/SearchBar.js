cee09c1ffc33326ebf3d4f1651e0b936b2905372
Matheus Marques <marquesm91@gmail.com>
2018-01-12 16:54:09 -0200

    refactor: Now SearchBar is a class component and has its own query state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const SearchBar = ({ onUpdateQuery, query, placeholder }) => (
-  <div className="search-books-bar">
-    <Link to='/' className='close-search'>Close</Link>
-    <div className="search-books-input-wrapper">
-      {/*
-        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-        You can find these search terms here:
-        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+class SearchBar extends Component {
+  state = {
+    query: ''
+  }
 
-        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-        you don't find a specific author or title. Every search is limited by search terms.
-      */}
-      <input
-        type="text"
-        placeholder={placeholder}
-        value={query}
-        onChange={event => onUpdateQuery(event.target.value)}
-      />
-    </div>
-  </div>
-);
+  onChangeQueryHandler = event => {
+    this.setState({ query: event.target.value.trim() });
+  }
+
+  onKeyPressHandler = event => {
+    if (event.key === 'Enter' && this.state.query) {
+      this.props.onEnterPressed(this.state.query)
+    }
+  }
+
+  render() {
+    return (
+      <div className="search-books-bar">
+        <Link to='/' className='close-search'>Close</Link>
+        <div className="search-books-input-wrapper">
+          {/*
+            NOTES: The search from BooksAPI is limited to a particular set of search terms.
+            You can find these search terms here:
+            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+            you don't find a specific author or title. Every search is limited by search terms.
+          */}
+          <input
+            type="text"
+            placeholder={this.props.placeholder}
+            value={this.state.query}
+            onKeyPress={this.onKeyPressHandler}
+            onChange={this.onChangeQueryHandler}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export { SearchBar };
