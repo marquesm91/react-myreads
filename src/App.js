60856e082e03347d1ecb76adfa37dd03b1a80c79
Matheus Marques <marquesm91@gmail.com>
2018-01-12 10:25:19 -0200

    refactor: books state are controled by App.js now

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 import ListBooks from './view/ListBooks';
 import SearchBooks from './view/SearchBooks';
 
 import './App.css'
 
 class MyReadsApp extends Component {
+  state = {
+    books: {}
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(books => {
+        // transform array into object and its key = book.id
+        let booksAsObject = books.reduce((acc, cur) => {
+          acc[cur.id] = cur;
+          return acc;
+        }, {});
+
+        this.setState({ books: booksAsObject });
+      });
+  }
+
+  setBookShelf = (book, shelf) => {
+    // Add book to books object and set its shelf properly
+    this.setState({
+      books: {
+        ...this.state.books,
+        [book.id]: {
+          ...book,
+          shelf: shelf
+        }
+      }
+    });
+
+    // Also updates shelf on server to persist data
+    BooksAPI.update(book, shelf);
+  }
+
   render() {
     return (
       <div className="app">
-        <Route exact path='/' component={ListBooks} />
+        <Route exact path='/' render={() => (
+          <ListBooks
+            books={this.state.books}
+            onChangeShelf={this.setBookShelf}
+          />
+        )} />
         <Route path='/search' component={SearchBooks} />
       </div>
     )
--- src/view/ListBooks.js
+++ src/view/ListBooks.js
@@ -1,57 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Shelf, HeaderPage } from '../components';
-import * as BooksAPI from '../BooksAPI';
+
 
 class ListBooks extends Component {
-  state = {
-    books: {}
-  }
-
-  componentDidMount() {
-    BooksAPI.getAll()
-      .then(books => {
-        // transform array into object and its key = book.id
-        let booksAsObject = books.reduce((acc, cur) => {
-          acc[cur.id] = cur;
-          return acc;
-        }, {});
-
-        this.setState({ books: booksAsObject });
-      });
-  }
-
   filterBooks(shelf) {
-    return Object.values(this.state.books).filter(book => book.shelf === shelf)
-  }
-
-  setBookShelf = (book, shelf) => {
-    // Add book to books object and set its shelf properly
-    this.setState({
-      books: {
-        ...this.state.books,
-        [book.id]: {
-          ...book,
-          shelf: shelf
-        }
-      }
-    });
-
-    // Also updates shelf on server to persist data
-    BooksAPI.update(book, shelf);
+    return Object.values(this.props.books).filter(book => book.shelf === shelf);
   }
 
   render() {
-    const { currentlyReadingBooks, wantToReadBooks, readBooks } = this.state;
+    const { onChangeShelf } = this.props;
 
     return (
       <div className="list-books">
         <HeaderPage title="My Reads" />
         <div className="list-books-content">
           <div>
-            <Shelf title="Currently Reading" books={this.filterBooks('currentlyReading')} onChangeShelf={this.setBookShelf} />
-            <Shelf title="Want to Read" books={this.filterBooks('wantToRead')} onChangeShelf={this.setBookShelf} />
-            <Shelf title="Read" books={this.filterBooks('read')} onChangeShelf={this.setBookShelf} />
+            <Shelf title="Currently Reading" books={this.filterBooks('currentlyReading')} onChangeShelf={(book, shelf) => onChangeShelf(book, shelf)} />
+            <Shelf title="Want to Read" books={this.filterBooks('wantToRead')} onChangeShelf={(book, shelf) => onChangeShelf(book, shelf)} />
+            <Shelf title="Read" books={this.filterBooks('read')} onChangeShelf={(book, shelf) => onChangeShelf(book, shelf)} />
           </div>
         </div>
         <div className="open-search">
