5fefea9e5b3dedefdcf7890b02ed8445bbf27d72
Matheus Marques <marquesm91@gmail.com>
2018-01-12 16:58:33 -0200

    bug: Fix search bar return wrong book and its own shelves if any

--- src/view/SearchBooks.js
+++ src/view/SearchBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { SearchBar, Book } from '../components';
+import { partition } from '../util';
 import * as BooksAPI from '../BooksAPI';
 
 class SearchBooks extends Component {
@@ -7,55 +8,35 @@ class SearchBooks extends Component {
     fetchedBooks: {}
   }
 
-  search = () => {
-    if (this.props.query) {
-      BooksAPI.search(this.props.query)
-        .then(fetchedBooks => {
-          let fetchedBooksAsObject = fetchedBooks.reduce((acc, cur) => {
-            cur.shelf = 'none';
-            acc[cur.id] = cur;
-            return acc;
-          }, {});
+  doSearch = query => {
+    BooksAPI.search(query).then(fetchedBooks => {
+      // partition fetchedBooks into books which belongs to any shelf or none
+      let [fetchedBooksOffMyShelf, fetchedBooksOnMyShelf] = partition(fetchedBooks, book => !this.props.books[book.id])
 
-          let books = [];
+      // Get local books state to show its shelf
+      let booksOnMyShelf = fetchedBooksOnMyShelf.map(({ id }) => this.props.books[id]);
 
-          fetchedBooks.filter(book => this.props.books[book.id]).map(({ id }) => {
-            BooksAPI.get(id).then(book => {
-              books.push({
-                [book.id]: {
-                  ...book
-                }
-              });
-
-              console.log(...books);
-              this.setState({
-                fetchedBooks: {
-                  ...fetchedBooksAsObject,
-                  ...books
-                }
-              });
-            });
-          })
-
-
-        });
-    }
+      this.setState({
+        fetchedBooks: {
+          ...fetchedBooksOffMyShelf,
+          ...booksOnMyShelf
+        }
+      });
+    });
   }
 
   render() {
+    const booksAsArray = Object.values(this.state.fetchedBooks);
+
     return (
       <div className="search-books">
         <SearchBar
-          placeholder="Search by title or author"
-          onUpdateQuery={value => this.props.onUpdateQuery(value)}
-          query={this.props.query}
+          placeholder="Search by title or author and hit enter"
+          onEnterPressed={query => this.doSearch(query)}
         />
-        <div className="open-search">
-          <button onClick={this.search}>Search</button>
-        </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {Object.values(this.state.fetchedBooks).length > 0 && Object.values(this.state.fetchedBooks).map(book => (
+            {booksAsArray.length > 0 && booksAsArray.map(book => (
               <li key={book.id} >
                 <Book book={book} onChangeShelf={(book, shelf) => this.props.onChangeShelf(book, shelf)}/>
               </li>
