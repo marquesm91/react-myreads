95e02164a74ecb381e5d97155f8d6613119fe31e
Matheus Marques <marquesm91@gmail.com>
2018-01-14 12:18:55 -0200

    refactor: ShelfChanger has a shelf state now

--- src/components/ShelfChanger.js
+++ src/components/ShelfChanger.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ShelfChanger = ({ onChangeShelf, shelf }) => (
-  <div className="book-shelf-changer">
-    <select
-      value={shelf ? shelf : 'none'}
-      onChange={event => onChangeShelf(event.target.value)}
-    >
-      <option value="none" disabled>Move to...</option>
-      <option value="currentlyReading">Currently Reading</option>
-      <option value="wantToRead">Want to Read</option>
-      <option value="read">Read</option>
-      <option value="none">None</option>
-    </select>
-  </div>
-);
+class ShelfChanger extends Component {
+  state = {
+    shelf: this.props.shelf || 'none'
+  }
+
+  onChangeShelfHandler = (event) => {
+    this.setState({
+      shelf: event.target.value
+    });
+
+    this.props.onChangeShelf(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="book-shelf-changer">
+        <select
+          value={this.state.shelf}
+          onChange={this.onChangeShelfHandler}
+        >
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
 
 export { ShelfChanger };
