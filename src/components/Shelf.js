acab85a58eff2a1f97cc648389e48118f64a5ce4
Matheus Marques <marquesm91@gmail.com>
2018-01-21 20:10:52 -0200

    feat: Update Shelf only when necessary

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { length } from '../util';
 import { BookList } from './index';
 
-const Shelf = ({ onChangeShelf, title, books }) => (
-  <div className="bookshelf">
-    <h2 className="bookshelf-title">{title}</h2>
-    <div className="bookshelf-books">
-      <BookList books={books} onChangeShelf={(book, shelf) => onChangeShelf(book, shelf)} />
-    </div>
-  </div>
-);
+class Shelf extends Component {
+  shouldComponentUpdate(nextProps) {
+    return length(nextProps.books) !== length(this.props.books);
+  }
+
+  render() {
+    const { onChangeShelf, title, books } = this.props;
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <BookList books={books} onChangeShelf={(book, shelf) => onChangeShelf(book, shelf)} />
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export { Shelf };
--- src/util/index.js
+++ src/util/index.js
@@ -1,12 +1,19 @@
 export const arrayToObject = (array, key) => {
   key = key || 'id';
-  
+
   return array.reduce((acc, cur) => {
     acc[cur[key]] = cur;
     return acc;
   }, {});
 };
 
-export const partition = (array, isValid) =>  array.reduce(([pass, fail], elem) => {
-  return isValid(elem) ? [[...pass, elem], fail] : [pass, [...fail, elem]];
-}, [[], []]);
+export const removeByKey = (obj, deleteKey) => {
+  return Object.keys(obj)
+    .filter(key => key !== deleteKey)
+    .reduce((result, current) => {
+      result[current] = obj[current];
+      return result;
+  }, {});
+}
+
+export const length = (obj) => Object.keys(obj).length;
