28f638136478286607028c1822b71f2e1df2388f
Matheus Marques <marquesm91@gmail.com>
2018-01-22 09:15:24 -0200

    refactor: Make spinner more customizable and define defaultProps

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Loading = ({ title }) => (
-  <div className="bookshelf-loading-container">
-    <div className="bookshelf-loading"></div>
-  </div>
-);
+const Loading = ({ size, color }) => {
+  return (
+    <div className="bookshelf-loading-container">
+      <div
+        className={`bookshelf-loading ${size}`}
+        style={{ borderTopColor: color }}
+      ></div>
+    </div>
+  );
+};
+
+Loading.propTypes = {
+  size: PropTypes.string,
+  color: PropTypes.string
+};
+
+Loading.defaultProps = {
+  size: 'large',
+  color: '#277c32'
+};
 
 export { Loading };
