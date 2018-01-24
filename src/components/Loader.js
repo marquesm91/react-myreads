6e3e4bfd0a02041408ce2deda5a4970d62ef0f5d
Matheus Marques <marquesm91@gmail.com>
2018-01-24 10:24:55 -0200

    refactor: Loader can have color options

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Loader = ({ size, color }) => {
+const Loader = ({ size, backgroundColor, loaderColor }) => {
   return (
     <div className="bookshelf-loader-container">
       <div
         className={`bookshelf-loader ${size}`}
-        style={{ borderTopColor: color }}
+        style={{
+          borderColor: backgroundColor,
+          borderTopColor: loaderColor
+        }}
       ></div>
     </div>
   );
 };
 
 Loader.propTypes = {
-  size: PropTypes.string,
-  color: PropTypes.string
+  size: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string,
+  loaderColor: PropTypes.string
 };
 
 Loader.defaultProps = {
   size: 'large',
-  color: '#277c32'
+  backgroundColor: '#fff',
+  loaderColor: '#277c32'
 };
 
 export { Loader };
