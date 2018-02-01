1691e5ef64026dc12eb7053b0c913213b7c73561
marquesm91 <marquesm91@gmail.com>
2018-02-01 18:17:34 -0200

    refactor: Shelf are now a stateless function component

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,31 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BookList, Loader } from './index';
 
-class Shelf extends Component {
-  shouldComponentUpdate(nextProps) {
-    if (!this.props.books) {
-      return true;
-    }
-
-    return nextProps.books.length !== this.props.books.length;
-  }
-
-  render() {
-    const { onChangeShelf, title, books } = this.props;
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          {books
-            ? <BookList books={books} onChangeShelf={(book, shelf) => onChangeShelf(book, shelf)} />
-            : <Loader />
-          }
-        </div>
-      </div>
-    );
-  }
-}
-
+const Shelf = ({ onChangeShelf, title, books }) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{title}</h2>
+    <div className="bookshelf-books">
+      {books
+        ? <BookList books={books} onChangeShelf={(book, shelf) => onChangeShelf(book, shelf)} />
+        : <Loader />
+      }
+    </div>
+  </div>
+);
 
 export { Shelf };
